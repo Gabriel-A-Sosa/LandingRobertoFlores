1726a9de47066a413ff7186e5930d6943bc120d5
Gabriel Sosa <107868872+Gabriel-A-Sosa@users.noreply.github.com>
2022-08-13 22:24:35 -0300

    Update reproductor.js

--- Scripts/reproductor.js
+++ Scripts/reproductor.js
@@ -1,4 +1,5 @@
-function cambiarTrack(track) {
+/* function cambiarTrack(track) {
+
     var path =  track.getAttribute("path")
     viejo_audio = document.getElementById("reproductor")
     audio_padre = viejo_audio.parentNode
@@ -13,10 +14,38 @@ function cambiarTrack(track) {
     source.setAttribute("id", "reproductorSource")
     nuevo_audio.appendChild(source)
     nuevo_audio.setAttribute("autoplay", "true")  
+    audio_padre.appendChild(nuevo_audio)} */
+
+function validarRadio(){
+	for(var i=0;i<document.Canciones.Espejos.length;i++){
+		if(document.Canciones.Espejos[i].checked){
+         
+	 var path = document.Canciones.Espejos[i].value;
+    viejo_audio = document.getElementById("reproductor")
+    audio_padre = viejo_audio.parentNode
+    audio_padre.removeChild(viejo_audio)
+    nuevo_audio = document.createElement("audio")
+    nuevo_audio.setAttribute("id","reproductor")
+    nuevo_audio.setAttribute("controls", "controls")
+   // nuevo_audio.setAttribute("autoplay", "autoplay")
+    source = document.createElement("source")
+    source.setAttribute("src", path)
+    source.setAttribute("type", "audio/mpeg")
+    source.setAttribute("id", "reproductorSource")
+    nuevo_audio.appendChild(source)
+    nuevo_audio.setAttribute("autoplay", "true")  
     audio_padre.appendChild(nuevo_audio)}
+    
+	
+	}
+	
+   }
+   var s="no";
+   var path = select.getAttribute("path");
+
+
  function cargarReproductor() {
-   var select = document.getElementById("selectTrack")
-   var path = select.options[0].getAttribute("path")
+   
     nuevo_audio = document.createElement("audio")
     nuevo_audio.setAttribute("id","reproductor")
     nuevo_audio.setAttribute("controls", "controls")       
@@ -28,6 +57,11 @@ function cambiarTrack(track) {
     padre = document.getElementById("reproductorBox")
     padre.appendChild(nuevo_audio)
  }
+
+ if(document.Canciones.Espejos[i].checked){
+   alert (document.Canciones.Espejos[i].value)
+ }
+
 
 
 
